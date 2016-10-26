be38b1b222b7f9e5fda26f385ae41ae24ac638eb
Ellis Clayton <ellis@ellisclayton.info>
2016-10-26 20:37:06 +1000

    Update MOTD with random Cowsay Cow

--- config/motd.sh
+++ config/motd.sh
@@ -1,1 +1,11 @@
-fortune -s | cowsay
+# Lovingly borrowed from https://github.com/bkendzior/cowfiles
+
+function cowsayfortune {
+  NUMOFCOWS=`cowsay -l | tail -n +2 | wc -w`
+  WHICHCOW=$((RANDOM%$NUMOFCOWS+1))
+  THISCOW=`cowsay -l | tail -n +2 | sed -e 's/\ /\'$'\n/g' | sed $WHICHCOW'q;d'`
+
+  fortune -s | cowsay -f $THISCOW -W 100
+}
+
+cowsayfortune
