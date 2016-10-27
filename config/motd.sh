c912bd013303d712640cf213788ae2a24a067304
Ellis Clayton <ellis@ellisclayton.info>
2016-10-27 18:08:58 +1000

    Refactor Cowsay Fortune

--- config/motd.sh
+++ config/motd.sh
@@ -1,11 +1,2 @@
-# Lovingly borrowed from https://github.com/bkendzior/cowfiles
-
-function cowsayfortune {
-  NUMOFCOWS=`cowsay -l | tail -n +2 | wc -w`
-  WHICHCOW=$((RANDOM%$NUMOFCOWS+1-3)) # Subtract number of excluded cows at end
-  THISCOW=`cowsay -l | tail -n +2 | tr " " "\n" | grep -Ev "bong|sodomized|head-in" | sed -e 's/\ /\'$'\n/g' | sed $WHICHCOW'q;d'`
-
-  fortune -s | cowsay -f $THISCOW
-}
-
 cowsayfortune
+
