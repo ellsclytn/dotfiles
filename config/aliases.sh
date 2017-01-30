da89ce54c514a710ec8fd675367c606e652cb15b
Ellis Clayton <ellis@ellisclayton.info>
2017-01-30 10:04:25 +1000

    Remove old aliases

--- config/aliases.sh
+++ config/aliases.sh
@@ -1,13 +1,10 @@
 alias ar="asciinema"
-alias ag="rg"
 alias dev="cd ~/Documents/Development"
 alias dotfiles="cd ~/.dotfiles"
 alias laralog="tail -f storage/logs/laravel.log"
 alias vim="nvim"
 alias vi="nvim"
 alias v="nvim"
-alias vimconfig="vim ~/.config/nvim/init.vim"
-alias zshconfig="vim ~/.zshrc"
 alias zshsource="source ~/.zshrc"
 
 function git() {
