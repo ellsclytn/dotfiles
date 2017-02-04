3ea9e617c539738f21182ac6be7217d89a07dd62
Ellis Clayton <ellis@ellisclayton.info>
2017-02-04 14:20:05 +1000

    Add composer alias

--- config/aliases.sh
+++ config/aliases.sh
@@ -1,10 +1,11 @@
 alias ar="asciinema"
+alias composer="php /usr/local/bin/composer.phar"
 alias dev="cd ~/Documents/Development"
 alias dotfiles="cd ~/.dotfiles"
 alias laralog="tail -f storage/logs/laravel.log"
-alias vim="nvim"
-alias vi="nvim"
 alias v="nvim"
+alias vi="nvim"
+alias vim="nvim"
 alias zshsource="source ~/.zshrc"
 
 function git() {
