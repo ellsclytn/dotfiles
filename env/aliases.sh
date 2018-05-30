5e47df75f9951b78f35d467d67a0266d95cba9c1
Ellis Clayton <ellis@ellisclayton.info>
2018-05-30 19:38:07 +1000

    Reorganise aliases 📒
    Moves OS detection logic to freshrc, so it only gets called during env
    compilation.
    
    Removes some useless aliases

--- env/aliases.sh
+++ env/aliases.sh
@@ -4,14 +4,8 @@ alias dev="cd ~/Code"
 alias dotfiles="cd ~/.dotfiles"
 alias g="hub"
 alias l='exa -hla --group-directories-first'
-alias laralog="tail -f storage/logs/laravel.log"
 alias typora="open -a typora"
 alias v="nvim"
 alias vi="nvim"
 alias vim="nvim"
 alias zshsource="source ~/.zshrc"
-
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  alias pbcopy='xclip -selection clipboard'
-  alias pbpaste='xclip -selection clipboard -o'
-fi
--- env/docker.sh
+++ env/docker.sh
@@ -1,4 +1,3 @@
 alias c="docker-compose"
 alias cup="docker-compose up"
 alias cupd="docker-compose up -d"
-alias cdown="docker-compose down"
