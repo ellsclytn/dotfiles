db2f01737c775623997658c1271b85ba573850d7
Ellis Clayton <ellis@ellisclayton.info>
2018-07-22 17:26:11 +1000

    Remove Hub alias

--- env/aliases.sh
+++ env/aliases.sh
@@ -2,7 +2,7 @@ alias :q="clear"
 alias ar="asciinema"
 alias dev="cd ~/Code"
 alias dotfiles="cd ~/.dotfiles"
-alias g="hub"
+alias g='git'
 alias l='exa -hl --group-directories-first --time-style=long-iso'
 alias la='l -a'
 alias typora="open -a typora"
