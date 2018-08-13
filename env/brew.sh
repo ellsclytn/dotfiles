c5bffcc0d7caafc57ee3b833b4b50fc0e3017492
Ellis Clayton <ellis@ellisclayton.info>
2018-08-13 22:46:32 +1000

    Remove unused rbenv init

--- env/brew.sh
+++ env/brew.sh
@@ -1,8 +1,5 @@
+#!/bin/bash
 export PATH="/usr/local/bin:$PATH"
 
 # Cask install directory
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-
-# Initialise rbenv for homebrew install
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-
