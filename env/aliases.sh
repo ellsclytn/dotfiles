5f5b6191b864fa3472075e34f574cfa5101c2f04
Ellis Clayton <ellis@ellisclayton.info>
2019-03-25 15:48:46 +1100

    Add git push force aliases

--- env/aliases.sh
+++ env/aliases.sh
@@ -3,6 +3,8 @@ alias ar="asciinema"
 alias dev="cd ~/Code"
 alias dotfiles="cd ~/.dotfiles"
 alias g='git'
+alias gpf='echo -e "\033[1;31m!!!Forcing!!!\033[0m" && sleep 3 && git push --force-with-lease'
+alias gpf!='echo "!!!Forcing (NO LEASE)!!!" && sleep 3 && git push --force'
 alias l='exa -hl --group-directories-first --time-style=long-iso'
 alias la='l -a'
 alias typora="open -a typora"
