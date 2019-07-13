fac888cc4f131cdcb233413c8f5404eb2cc623bc
Ellis Clayton <ellis@ellisclayton.info>
2019-07-13 22:34:57 +1000

    Switch to LSD 🌈
    Until the exa thing is fixed

--- env/aliases.sh
+++ env/aliases.sh
@@ -5,8 +5,10 @@ alias dotfiles="cd ~/.dotfiles"
 alias g='git'
 alias gpf='echo -e "\033[1;31m!!!Forcing!!!\033[0m" && sleep 3 && git push --force-with-lease'
 alias gpf!='echo "!!!Forcing (NO LEASE)!!!" && sleep 3 && git push --force'
-alias l='exa -hl --group-directories-first --time-style=long-iso'
-alias la='l -a'
+# Until exa is fix https://github.com/ogham/exa/issues/517
+# alias l='exa -hl --group-directories-first --time-style=long-iso'
+alias l='lsd -l --icon never --group-dirs first --date relative -F'
+alias la='l -A'
 alias typora="open -a typora"
 alias v="nvim"
 alias vi="nvim"
