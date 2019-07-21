36c7c6caf45f8218b2cb628ad5624af656bc09c8
Ellis Clayton <ellis@ellisclayton.info>
2019-07-22 08:44:09 +1000

    Revert "Switch to LSD"
    Exa has been fixed now
    
    This reverts commit fac888cc4f131cdcb233413c8f5404eb2cc623bc.

--- env/aliases.sh
+++ env/aliases.sh
@@ -5,10 +5,8 @@ alias dotfiles="cd ~/.dotfiles"
 alias g='git'
 alias gpf='echo -e "\033[1;31m!!!Forcing!!!\033[0m" && sleep 3 && git push --force-with-lease'
 alias gpf!='echo "!!!Forcing (NO LEASE)!!!" && sleep 3 && git push --force'
-# Until exa is fix https://github.com/ogham/exa/issues/517
-# alias l='exa -hl --group-directories-first --time-style=long-iso'
-alias l='lsd -l --icon never --group-dirs first --date relative -F'
-alias la='l -A'
+alias l='exa -hl --group-directories-first --time-style=long-iso'
+alias la='l -a'
 alias v="nvim"
 alias vi="nvim"
 alias vim="nvim"
