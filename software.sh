dd3648ac54ef921cd58cfd1880e7a6ccc8244434
Ellis Clayton <ellis@ellisclayton.info>
2019-08-10 18:08:46 +1000

    Add Gomplate
    Useful as a means of adding templating to all my config files (for
    mac/linux specific stuff, etc.)

--- software.sh
+++ software.sh
@@ -15,6 +15,13 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# Gomplate (done via Brew on macOS)
+if ! [[ "$(uname)" == "Darwin" ]]; then
+  GOMPLATE_VERSION=$(github-release hairyhenderson/gomplate)
+  sudo curl -sL https://github.com/hairyhenderson/gomplate/releases/download/$GOMPLATE_VERSION/gomplate_linux-amd64-slim -o /usr/local/bin/gomplate
+  sudo chmod +x /usr/local/bin/gomplate
+fi
+
 # #Fresh
 bash -c "`curl -sL get.freshshell.com`"
 rm ~/.freshrc
