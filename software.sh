c011f63cdb95daa0b3fec4409e06f673b6c7e75d
Ellis Clayton <ellis@ellisclayton.info>
2019-08-10 16:21:49 +1000

    Fix NVM installer
    They've changed the repo path

--- software.sh
+++ software.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Nvm
-NVM_VERSION=$(github-release creationix/nvm)
-curl -o- https://raw.githubusercontent.com/creationix/nvm/$NVM_VERSION/install.sh | bash
+NVM_VERSION=$(github-release nvm-sh/nvm)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$NVM_VERSION/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install node
