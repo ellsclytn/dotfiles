f0ba474854bb64e69ab4876c286d20e89db4a38e
Ellis Clayton <ellis@ellisclayton.info>
2019-08-19 21:27:59 +1000

    Add git status to `l`

--- env/aliases.sh
+++ env/aliases.sh
@@ -5,7 +5,7 @@ alias dotfiles="cd ~/.dotfiles"
 alias g='git'
 alias gpf='echo -e "\033[1;31m!!!Forcing!!!\033[0m" && sleep 3 && git push --force-with-lease'
 alias gpf!='echo "!!!Forcing (NO LEASE)!!!" && sleep 3 && git push --force'
-alias l='exa -hl --group-directories-first --time-style=long-iso'
+alias l='exa -hl --group-directories-first --git --time-style=long-iso'
 alias la='l -a'
 alias v="nvim"
 alias vi="nvim"
