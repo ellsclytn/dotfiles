1563b72c16e72c84fcc8820b8e8218726ad69b59
Ellis Clayton <ellis@ellis.codes>
2020-01-30 12:19:40 +1100

    Include hidden files in fzf results

--- env/fzf.sh
+++ env/fzf.sh
@@ -1,3 +1,3 @@
-export FZF_DEFAULT_COMMAND='fd --type f --color=never'
+export FZF_DEFAULT_COMMAND='fd --type f --color=never --hidden'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND='fd --type d . --color=never'
+export FZF_ALT_C_COMMAND='fd --type d . --color=never --hidden'
