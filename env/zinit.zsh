1192d7cff0f0c3d7644e4a1535c9a3a6689b91ed
Ellis Clayton <ellis@ellis.codes>
2020-03-05 13:21:57 +1100

    Use turbo mode for zinit plugin loading
    Is fast. Very fast.
    
    https://zdharma.org/zinit/wiki/INTRODUCTION/#turbo_mode_zsh_62_53

--- env/zinit.zsh
+++ env/zinit.zsh
@@ -2,29 +2,29 @@ source "${HOME}/.zinit/bin/zinit.zsh"
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
 
-zinit ice silent wait"0" blockf
+zinit ice silent wait'!0' blockf
 zinit light zsh-users/zsh-completions
 
-zinit ice silent wait"0" atload"_zsh_autosuggest_start"
+zinit ice silent wait'!0' atload"_zsh_autosuggest_start"
 zinit light zsh-users/zsh-autosuggestions
 
-zinit ice silent wait"0"
+zinit ice silent wait'!0'
 zinit snippet https://github.com/gantsign/zsh-plugins/blob/master/ctop/_ctop
 
-zinit ice silent wait"0" atinit"zpcompinit; zpcdreplay"
+zinit ice silent wait'!0' atinit"zpcompinit; zpcdreplay"
 zinit light zdharma/fast-syntax-highlighting
 
-zinit ice silent wait"0"
+zinit ice silent wait'!0'
 zinit light djui/alias-tips
 
-zinit ice silent wait"0"
+zinit ice silent wait'!0'
 zinit light lukechilds/zsh-nvm
 
-zinit ice silent wait"0"
+zinit ice silent wait'!0'
 zinit light lukechilds/zsh-better-npm-completion
 
 zinit snippet OMZ::plugins/git/git.plugin.zsh
 
 # CLI Emojifier ⭐
-zinit ice as"program" cp"zemojify -> emojify" pick"emojify"
+zinit ice silent wait'!0' as"program" cp"zemojify -> emojify" pick"emojify"
 zinit light filipekiss/zemojify
