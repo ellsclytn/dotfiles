5ce16ad19de72372d994aa57b4ab5293e8428c6e
Ellis Clayton <ellis@ellis.codes>
2020-03-31 13:42:05 +1100

    Switch from z to zoxide
    It's a little bit cleaner to integrate with fzf

--- env/aliases.sh
+++ env/aliases.sh
@@ -2,6 +2,7 @@ alias :q="clear"
 alias ar="asciinema"
 alias dev="cd ~/Code"
 alias dotfiles="cd ~/.dotfiles"
+alias f="zi"
 alias g='git'
 alias gpf='echo -e "\033[1;31m!!!Forcing!!!\033[0m" && sleep 3 && git push --force-with-lease'
 alias gpf!='echo "!!!Forcing (NO LEASE)!!!" && sleep 3 && git push --force'
