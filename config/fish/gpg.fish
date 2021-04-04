cf7eb448d760dc0f7473c9011b33e038c6721f85
Ellis Clayton <ellis@ellis.codes>
2021-04-04 14:01:33 +1000

    Avoid setting GPG and SSH variables unnecessarily
    Noninteractive shells shouldn't be trying to do GPG things without my
    knowledge

--- config/fish/gpg.fish
+++ config/fish/gpg.fish
@@ -1,3 +1,5 @@
-set -x GPG_TTY (tty)
-set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-gpg-connect-agent updatestartuptty /bye > /dev/null
+if status is-interactive
+  set -x GPG_TTY (tty)
+  set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+  gpg-connect-agent updatestartuptty /bye > /dev/null
+end
