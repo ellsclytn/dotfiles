3596c98a85b9941177ff875cf5a199a7a87f3d36
Ellis Clayton <ellis@ellis.codes>
2021-04-04 12:18:45 +1000

    Switch tab completion mode
    Mode 3 is too afar from what I'm used to.
    
    https://github.com/jethrokuan/fzf/wiki/FZF-Tab-Completions

--- config/fish/fzf.fish
+++ config/fish/fzf.fish
@@ -5,4 +5,4 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND='fd --type d . --color=never --hidden'
 
 set -U FZF_LEGACY_KEYBINDINGS 0
-set -U FZF_COMPLETE 3
+set -U FZF_COMPLETE 2
