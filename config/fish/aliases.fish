3056a3a4c1cd48a463029672d6613fb75c5f1606
Ellis Clayton <ellis@ellis.codes>
2021-04-04 16:39:35 +1000

    Fix coloured output of aliases
    Bash is hard. `gpf!` wasn't colouring the output when I attempted to use
    Bash colouring codes on it, but Fish makes it easier.

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -6,8 +6,8 @@ alias dotfiles="cd ~/.dotfiles"
 alias du="dust"
 alias f="zi"
 alias g='git'
-alias gpf='echo -e "\033[1;31m!!!Forcing!!!\033[0m" && sleep 3 && git push --force-with-lease'
-alias gpf!='echo "!!!Forcing (NO LEASE)!!!" && sleep 3 && git push --force'
+alias gpf='set_color -o yellow; echo "Force pushing"; set_color normal && sleep 3 && git push --force-with-lease'
+alias gpf!='set_color -o yellow; echo -n "Force pushing "; set_color -o red; echo "without lease"; set_color normal && sleep 3 && git push --force'
 alias l='exa -hl --group-directories-first --git --time-style=long-iso'
 alias la='l -a'
 alias v="nvim"
