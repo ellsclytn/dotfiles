c02ea2fa0265970ab939e52b9b954cc7acf71199
Ellis Clayton <ellis@ellis.codes>
2021-04-24 10:17:17 +1000

    Add interactive container & image deletion

--- config/fish/docker.fish
+++ config/fish/docker.fish
@@ -2,4 +2,6 @@ if status --is-interactive
   abbr --add --global c "docker-compose"
   abbr --add --global cup "docker-compose up"
   abbr --add --global cupd "docker-compose up -d"
+  abbr --add --global drmi "docker images -a | fzf --header-lines=1 -m | awk \"{print \$3}\" | xargs docker rmi"
+  abbr --add --global drmc "docker ps -a | fzf --header-lines=1 -m | awk \"{print \$1}\" | xargs docker rm -v"
 end
