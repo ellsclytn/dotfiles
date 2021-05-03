e93cdb3e11a46356bf08eae1f44db278f6e69fbc
Ellis Clayton <ellis@ellis.codes>
2021-05-04 09:25:36 +1000

    Expose image viewer as executable
    Unlike abbreviations, I can set file associations with executables

--- config/fish/feh.fish
+++
@@ -1,4 +0,0 @@
-# Sensible image viewer
-if status --is-interactive
-  abbr --add --global img "feh --scale-down --image-bg black --sort name --no-menus --conversion-timeout 1 --force-aliasing --title '[%u/%l] %n (%wx%h)'"
-end
