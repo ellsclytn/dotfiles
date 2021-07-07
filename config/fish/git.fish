1966f4ff3555d2ea9254c13e75f4f5cd1dbb4fca
Ellis Clayton <ellis@ellis.codes>
2021-07-07 16:09:29 +1000

    Migrate more git aliases to fish abbreviations

--- config/fish/git.fish
+++ config/fish/git.fish
@@ -1,8 +1,9 @@
 if status --is-interactive
   abbr --add --global g 'git'
-  abbr --add --global gsta "git stash"
-  abbr --add --global gstap "git stash pop"
+  abbr --add --global gl "git pull"
   abbr --add --global grb "git rebase"
   abbr --add --global grba "git rebase --abort"
   abbr --add --global grbc "git rebase --continue"
+  abbr --add --global gsta "git stash"
+  abbr --add --global gstap "git stash pop"
 end
