38080ce66785d6f2463a4ec7a7b62aeca0c66d30
Ellis Clayton <ellis@ellis.codes>
2022-02-10 17:37:41 +1100

    Sort plugins list

--- config/nvim/lua/_plugins.lua
+++ config/nvim/lua/_plugins.lua
@@ -2,15 +2,15 @@ return require('packer').startup(function()
     use 'wbthomason/packer.nvim'
 
     -- UI & Syntax
+    use 'editorconfig/editorconfig-vim'
     use 'navarasu/onedark.nvim'
     use 'nvim-treesitter/nvim-treesitter'
     use 'tpope/vim-fugitive' -- Git
-    use {'nvim-telescope/telescope.nvim', requires = {'nvim-lua/plenary.nvim'}}
-    use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'}}
-    use {'kyazdani42/nvim-tree.lua', requires = {'kyazdani42/nvim-web-devicons'}}
-    use 'editorconfig/editorconfig-vim'
     use {'SmiteshP/nvim-gps'}
+    use {'kyazdani42/nvim-tree.lua', requires = {'kyazdani42/nvim-web-devicons'}}
+    use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'}}
     use {'nvim-lualine/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons'}}
+    use {'nvim-telescope/telescope.nvim', requires = {'nvim-lua/plenary.nvim'}}
 
     -- Text operations
     use 'AndrewRadev/splitjoin.vim'
