d059472d7690a6322b849ee12d6eca670e78222b
Ellis Clayton <ellis@ellis.codes>
2022-04-06 19:39:12 +1000

    Remove Task

--- config/fish/task.fish
+++
@@ -1,7 +0,0 @@
-if status --is-interactive
-  abbr --add --global t "task"
-  abbr --add --global ta "task add"
-  abbr --add --global td "task done"
-  abbr --add --global tl "task list"
-  abbr --add --global ts "task sync"
-end
