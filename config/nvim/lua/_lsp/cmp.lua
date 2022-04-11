baf5cadfa3c4e77785bc7b48e6a01e3cd6af573a
Ellis Clayton <ellis@ellis.codes>
2022-04-11 21:37:34 +1000

    Add luasnip
    A snippet engine is required by cmp to make any completions work

--- config/nvim/lua/_lsp/cmp.lua
+++ config/nvim/lua/_lsp/cmp.lua
@@ -1,23 +1,37 @@
-local cmp = require('cmp')
+local present, cmp = pcall(require, 'cmp')
+
+if not present then
+    return
+end
+
 local lspkind = require('lspkind')
+local luasnip = require('luasnip')
+
 cmp.setup({
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end,
+    },
     formatting = {
         format = lspkind.cmp_format({
             mode = 'symbol', -- show only symbol annotations
         }),
     },
     mapping = {
-        ['<Tab>'] = cmp.mapping.select_prev_item(),
-        ['<S-Tab>'] = cmp.mapping.select_next_item(),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
         ['<CR>'] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
         }),
+        ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
         ['<Tab>'] = function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
             else
                 fallback()
             end
@@ -25,6 +39,8 @@ cmp.setup({
         ['<S-Tab>'] = function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
             else
                 fallback()
             end
@@ -32,5 +48,8 @@ cmp.setup({
     },
     sources = {
         { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'buffer' },
+        { name = 'path' },
     },
 })
