4a62864c226f2c1f67812826847fb2b8fe0b83a3
Ellis Clayton <ellis@ellis.codes>
2022-04-15 20:38:16 +1000

    Run eslint after prettier
    Most projects tend to use eslint, but not as many seem to use Prettier.
    This is especially true of what is run in CI linting processes. Thus,
    running eslint last is more likely to ensure confirmance with a given
    project's code style.

--- config/nvim/lua/_lsp/null-ls.lua
+++ config/nvim/lua/_lsp/null-ls.lua
@@ -4,8 +4,8 @@ local on_attach = require('_lsp.on_attach')
 null_ls.setup({
     sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.eslint_d,
         null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.eslint_d,
     },
     on_attach = on_attach,
 })
