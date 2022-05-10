22993862a9a1a6bb15b1c181c6a01d9738a6df7c
Ellis Clayton <ellis@ellis.codes>
2022-05-11 08:53:43 +1000

    Fix deprecation notice
    `on_server_ready` is being deprecated in favour of a more LSP-native API
    
    https://github.com/williamboman/nvim-lsp-installer/discussions/636

--- config/nvim/lua/_lsp/init.lua
+++ config/nvim/lua/_lsp/init.lua
@@ -2,8 +2,18 @@ local lspconfig = require('lspconfig')
 local lsp_installer = require('nvim-lsp-installer')
 local lsp_on_attach = require('_lsp.on_attach')
 
+lsp_installer.setup({
+    ensure_installed = {
+        'rust_analyzer',
+        'tsserver',
+        'terraformls',
+        'tflint',
+    },
+})
+
 lspconfig.tsserver.setup({
     on_attach = function(client)
+        -- Formatting is handled by null-ls (eslint and prettier)
         client.resolved_capabilities.document_formatting = false
         client.resolved_capabilities.document_range_formatting = false
     end,
@@ -13,23 +23,13 @@ lspconfig.tsserver.setup({
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
--- Register a handler that will be called for each installed server when it's ready (i.e. when installation is finished
--- or if the server is already installed).
-lsp_installer.on_server_ready(function(server)
-    local opts = {
-        on_attach = lsp_on_attach,
-    }
-
-    -- (optional) Customize the options passed to the server
-    -- if server.name == "tsserver" then
-    --     opts.root_dir = function() ... end
-    -- end
-
-    -- This setup() function will take the provided server configuration and decorate it with the necessary properties
-    -- before passing it onwards to lspconfig.
-    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-    server:setup(opts)
-end)
+for _, server in ipairs({
+    'rust_analyzer',
+    'terraformls',
+    'tflint',
+}) do
+    lspconfig[server].setup({ on_attach = lsp_on_attach })
+end
 
 require('_lsp.null-ls')
 require('_lsp.cmp')
