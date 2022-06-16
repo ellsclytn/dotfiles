da72d32f4284ac55f14ddcb2b1162d71d95baa71
Ellis Clayton <ellis@ellis.codes>
2022-06-16 10:36:51 +1000

    Convert to lua keymaps
    Recently introduced to Neovim
    
    https://github.com/neovim/neovim/pull/16591

--- config/nvim/lua/_mappings.lua
+++ config/nvim/lua/_mappings.lua
@@ -1,12 +1,46 @@
-function nmap(keys, command)
-    vim.api.nvim_set_keymap('n', keys, command, { noremap = true, silent = true })
-end
+-- Leader key
+vim.g.mapleader = ';'
 
-function vmap(keys, command)
-    vim.api.nvim_set_keymap('v', keys, command, { noremap = true, silent = true })
-end
+-- Copy to system clipboard
+vim.keymap.set('v', '<leader>y', '"+y')
 
-function _G.toggle_format_on_save()
+-- NvimTree
+vim.keymap.set('n', '<C-N>', ':NvimTreeToggle<CR>')
+vim.keymap.set('n', '<C-F>', ':NvimTreeFindFile<CR>')
+
+-- Toggle relative/absolute line numbers
+vim.keymap.set('n', '<C-L>', ':setlocal relativenumber!<cr>')
+
+-- Telescope
+vim.keymap.set('n', '<C-P>', ':Telescope find_files hidden=true<CR>')
+vim.keymap.set('n', '<C-G>', ':Telescope live_grep<CR>')
+vim.keymap.set('n', '<C-B>', ':Telescope buffers<CR>')
+vim.keymap.set('n', '<leader>fh', '<CMD>Telescope help_tags<CR>') -- Search help topics. Mnemonic: "fh for Find Help"
+
+-- Window nav similar to i3 navigation
+vim.keymap.set('n', '<C-J>', '<C-W><C-J>')
+vim.keymap.set('n', '<C-K>', '<C-W><C-K>')
+vim.keymap.set('n', '<C-L>', '<C-W><C-L>')
+vim.keymap.set('n', '<C-H>', '<C-W><C-H>')
+
+-- Double space over word to Subvert
+vim.keymap.set('n', '<Space><Space>', ':%S/<C-r>=expand("<cword>")<CR>/')
+-- Leader + space over word to find and replace
+vim.keymap.set('n', '<leader><Space>', ':%s/<C-r>=expand("<cword>")<CR>/')
+
+-- Reselect visual block after indent
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
+
+-- LSP
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
+vim.keymap.set('n', 'gr', vim.lsp.buf.references)
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation)
+vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help)
+vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename)
+vim.keymap.set('n', '<leader>fos', function()
     if vim.b.format_on_save then
         vim.b.format_on_save = false
         print('Format on save disabled')
@@ -14,53 +48,17 @@ function _G.toggle_format_on_save()
         vim.b.format_on_save = true
         print('Format on save enabled')
     end
-end
+end)
 
--- Leader key
-vim.g.mapleader = ';'
-
--- Copy to system clipboard
-vmap('<leader>y', '"+y')
-
--- NvimTree
-nmap('<C-N>', ':NvimTreeToggle<CR>')
-nmap('<C-F>', ':NvimTreeFindFile<CR>')
-
--- Toggle relative/absolute line numbers
-nmap('<C-L>', ':setlocal relativenumber!<cr>')
-
--- Telescope
-nmap('<C-P>', ':Telescope find_files hidden=true<CR>')
-nmap('<C-G>', ':Telescope live_grep<CR>')
-nmap('<C-B>', ':Telescope buffers<CR>')
-nmap('<leader>fh', '<CMD>Telescope help_tags<CR>') -- Search help topics. Mnemonic: "fh for Find Help"
-
--- Window nav similar to i3 navigation
-nmap('<C-J>', '<C-W><C-J>')
-nmap('<C-K>', '<C-W><C-K>')
-nmap('<C-L>', '<C-W><C-L>')
-nmap('<C-H>', '<C-W><C-H>')
-
--- Double space over word to Subvert
-nmap('<Space><Space>', ':%S/<C-r>=expand("<cword>")<CR>/')
--- Leader + space over word to find and replace
-nmap('<leader><Space>', ':%s/<C-r>=expand("<cword>")<CR>/')
-
--- Reselect visual block after indent
-vmap('<', '<gv')
-vmap('>', '>gv')
-
--- LSP
-nmap('gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
-nmap('gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
-nmap('gr', '<cmd>lua vim.lsp.buf.references()<CR>')
-nmap('gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
-nmap('K', '<cmd>lua vim.lsp.buf.hover()<CR>')
-nmap('<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
-nmap('<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
-nmap('<leader>fos', '<cmd>lua toggle_format_on_save()<CR>')
-
-nmap('<leader>S', "<cmd>lua require('spectre').open()<CR>")
-nmap('<leader>sw', "<cmd>lua require('spectre').open_visual({select_word=true})<CR>")
-vmap('<leader>s', "<cmd>lua require('spectre').open_visual()<CR>")
-nmap('<leader>sp', "<cmd>lua require('spectre').open_file_search()<cr>")
+vim.keymap.set('n', '<leader>S', function()
+    require('spectre').open()
+end)
+vim.keymap.set('n', '<leader>sw', function()
+    require('spectre').open_visual({ select_word = true })
+end)
+vim.keymap.set('v', '<leader>s', function()
+    require('spectre').open_visual()
+end)
+vim.keymap.set('n', '<leader>sp', function()
+    require('spectre').open_file_search()
+end)
