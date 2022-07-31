bf54ca5eee530d62cbb6c1b65d7a65e7362f5131
Ellis Clayton <ellis@ellis.codes>
2022-07-31 10:10:50 +1000

    Fix broken abbreviations

--- config/fish/docker.fish
+++ config/fish/docker.fish
@@ -2,8 +2,8 @@ if status --is-interactive
   abbr --add --global c "docker compose"
   abbr --add --global cup "docker compose up"
   abbr --add --global cupd "docker compose up -d"
-  abbr --add --global drmi "docker images -a | fzf --header-lines=1 -m | awk \"{print \$3}\" | xargs docker rmi"
-  abbr --add --global drmc "docker ps -a | fzf --header-lines=1 -m | awk \"{print \$1}\" | xargs docker rm -v"
+  abbr --add --global drmi "docker images -a | fzf --header-lines=1 -m | awk '{print \$3}' | xargs docker rmi"
+  abbr --add --global drmc "docker ps -a | fzf --header-lines=1 -m | awk '{print \$1}' | xargs docker rm -v"
 end
 
 set -gx DOCKER_BUILDKIT 1
