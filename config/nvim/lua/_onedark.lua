3755c1c684580325558b968389d84e69bfdb06ee
Ellis Clayton <ellis@ellis.codes>
2023-04-11 21:31:02 +1000

    Make syntax highlighting consistent between semantic/treesitter
    I'm not sure why they're different, but making all the @lsp.* groups
    linked to the treesitter groups grants consistent colours.
    
    From https://gist.github.com/swarn/fb37d9eefe1bc616c2a7e476c0bc0316#changing-highlights

--- config/nvim/lua/_onedark.lua
+++ config/nvim/lua/_onedark.lua
@@ -13,4 +13,24 @@ return function()
     })
 
     onedark.load()
+
+    local links = {
+        ['@lsp.type.namespace'] = '@namespace',
+        ['@lsp.type.type'] = '@type',
+        ['@lsp.type.class'] = '@type',
+        ['@lsp.type.enum'] = '@type',
+        ['@lsp.type.interface'] = '@type',
+        ['@lsp.type.struct'] = '@structure',
+        ['@lsp.type.parameter'] = '@parameter',
+        ['@lsp.type.variable'] = '@variable',
+        ['@lsp.type.property'] = '@property',
+        ['@lsp.type.enumMember'] = '@constant',
+        ['@lsp.type.function'] = '@function',
+        ['@lsp.type.method'] = '@method',
+        ['@lsp.type.macro'] = '@macro',
+        ['@lsp.type.decorator'] = '@function',
+    }
+    for newgroup, oldgroup in pairs(links) do
+        vim.api.nvim_set_hl(0, newgroup, { link = oldgroup, default = true })
+    end
 end
