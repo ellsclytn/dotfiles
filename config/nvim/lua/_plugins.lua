449b68995f1abe70833c027252ecd90c843be126
Ellis Clayton <ellis@ellis.codes>
2023-07-10 11:56:33 +1000

    Disable Copilot on some filetypes

--- config/nvim/lua/_plugins.lua
+++ config/nvim/lua/_plugins.lua
@@ -26,6 +26,22 @@ return {
             require('copilot').setup({
                 suggestion = { enabled = false },
                 panel = { enabled = false },
+                filetypes = {
+                    -- disable for files without filetype, such as a new buffer
+                    [''] = false,
+                    gitcommit = false,
+                    markdown = false,
+                    text = false,
+                    sh = function()
+                        if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '%.env.*') then
+                            -- disable for .env files
+                            print('copilot disabled for .env files')
+                            return false
+                        end
+
+                        return true
+                    end,
+                },
             })
         end,
     },
