348825d6d054b85631f89caf60baa9a8bac69697
Ellis Clayton <ellis@ellis.codes>
2023-09-09 09:20:03 +1000

    Replace exa with eza
    Exa is unmaintained, eza is a maintained fork.
    
    https://github.com/ogham/exa/issues/1243

--- config/fish/conf.d/aliases.fish
+++ config/fish/conf.d/aliases.fish
@@ -1,5 +1,5 @@
 alias ansible-system="cd ~/.dotfiles; sudo -E ansible-playbook ansible/playbook.yml; cd -"
 alias gpf='set_color -o yellow; echo "Force pushing"; set_color normal && sleep 3 && git push --force-with-lease --force-if-includes'
 alias gpf!='set_color -o yellow; echo -n "Force pushing "; set_color -o red; echo "without lease"; set_color normal && sleep 3 && git push --force'
-alias l='exa -hl --group-directories-first --git --time-style=long-iso'
+alias l='eza -hl --group-directories-first --git --time-style=long-iso'
 alias la='l -a'
