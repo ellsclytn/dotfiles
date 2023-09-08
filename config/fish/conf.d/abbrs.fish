29b44f14dca0096b22d1d3f6ee47579625706c79
Ellis Clayton <ellis@ellis.codes>
2023-09-09 09:28:40 +1000

    Replace dog with doggo
    Dog has gone unmaintained. While doggo isn't advertised as a drop in
    replacement, it seems to be close enough that I'm comfortable adding an
    abbreviation for dog through to doggo.
    
    https://github.com/ogham/dog/issues/97

--- config/fish/conf.d/abbrs.fish
+++ config/fish/conf.d/abbrs.fish
@@ -2,6 +2,7 @@ if status --is-interactive
   abbr --add --global :q "clear"
   abbr --add --global ar "asciinema"
   abbr --add --global dev "cd ~/Code"
+  abbr --add --global dog "doggo"
   abbr --add --global dotfiles "cd ~/.dotfiles"
   abbr --add --global du "dust"
   abbr --add --global f "zi"
