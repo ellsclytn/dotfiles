b9e18c17125772bb02b25ed84f91d10c9cbbb22c
Ellis Clayton <ellis@ellis.codes>
2024-03-01 16:32:03 +1100

    Add snippets to nvim cmp completions

--- config/nvim/lua/_lsp/cmp.lua
+++ config/nvim/lua/_lsp/cmp.lua
@@ -4,6 +4,8 @@ if not present then
     return
 end
 
+require('luasnip.loaders.from_vscode').lazy_load()
+
 local lspkind = require('lspkind')
 local luasnip = require('luasnip')
 
@@ -27,7 +29,7 @@ cmp.setup({
         }),
         ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
         ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-        ['<Tab>'] = function(fallback)
+        ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
@@ -35,8 +37,8 @@ cmp.setup({
             else
                 fallback()
             end
-        end,
-        ['<S-Tab>'] = function(fallback)
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
             elseif luasnip.jumpable(-1) then
@@ -44,7 +46,7 @@ cmp.setup({
             else
                 fallback()
             end
-        end,
+        end, { 'i', 's' }),
     },
     sources = {
         { name = 'copilot' },
--- config/nvim/lua/_plugins.lua
+++ config/nvim/lua/_plugins.lua
@@ -186,6 +186,14 @@ return {
     },
 
     {
+        'L3MON4D3/LuaSnip',
+        -- follow latest release.
+        version = 'v2.*', -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = 'make install_jsregexp',
+        dependencies = { 'rafamadriz/friendly-snippets' },
+    },
+    {
         'hrsh7th/nvim-cmp',
         event = 'VeryLazy',
         dependencies = {
