9095bcf666b9aec404e0e9bd302c71e975a22191
Ellis Clayton <ellis@ellis.codes>
2024-03-09 11:29:24 +1100

    Use vim.filetype.add for all filetypes
    Having the two different methods in use was redundant

--- config/nvim/lua/_filetypes.lua
+++ config/nvim/lua/_filetypes.lua
@@ -1,16 +1,11 @@
-vim.cmd([[
-augroup filetypedetect
-  au! BufRead,BufNewFile */ansible/*.yml setf yaml.ansible
-  au! BufRead,BufNewFile *.envrc setf sh
-augroup END
-]])
-
 vim.filetype.add({
     extension = {
         i3config = 'i3config',
         tf = 'terraform',
     },
     pattern = {
+        ['.*envrc.*'] = 'sh',
         ['.*gitconfig.*'] = 'gitconfig',
+        ['.*/ansible/.*.ya?ml'] = 'yaml.ansible',
     },
 })
