7a2b948f130414e4eb98b19a09a58bc7a2a7f94d
Ellis Clayton <ellis@ellis.codes>
2024-03-09 11:31:48 +1100

    Add syntax highlighting for X11 config
    It doesn't seem to work with `vim.filetype.add` on neovim 0.9.5, but
    worked fine when testing on a nightly build (NVIM v0.10.0-dev-2534+g25124d360).

--- config/nvim/lua/_filetypes.lua
+++ config/nvim/lua/_filetypes.lua
@@ -1,3 +1,10 @@
+-- TODO: Switch to `vim.filetype.add` once nvim v0.10 is released.
+vim.cmd([[
+augroup filetypedetect
+  au! BufRead,BufNewFile */X11/*.conf setf xf86conf
+augroup END
+]])
+
 vim.filetype.add({
     extension = {
         i3config = 'i3config',
