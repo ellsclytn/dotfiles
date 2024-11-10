550a299b65e2eb128ba3edb9b0167f4e6c91070e
Ellis Clayton <ellis@ellis.codes>
2024-11-10 21:07:19 +1100

    Switch to Sway

--- config/fish/linux.conf.d/pb.fish
+++ config/fish/linux.conf.d/pb.fish
@@ -1,5 +1,5 @@
 # Copy/paste clipboard - functions identically to macOS
 if status --is-interactive
-  abbr --add --global pbcopy 'xclip -selection clipboard'
-  abbr --add --global pbpaste 'xclip -selection clipboard -o'
+  abbr --add --global pbcopy 'wl-copy'
+  abbr --add --global pbpaste 'wl-paste'
 end
--- config/fish/linux.conf.d/startx.fish
+++ config/fish/linux.conf.d/startx.fish
@@ -1,7 +1,9 @@
 if status is-login
-    if test -z "$DISPLAY" -a -z "$INITIAL_SETUP" -a "$XDG_VTNR" = 1
+    if test -z "$WAYLAND_DISPLAY" -a -z "$INITIAL_SETUP" -a "$XDG_VTNR" = 1
         gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+        gsettings set org.gnome.desktop.interface font-name 'Source Sans 3 11'
         gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Dark'
-        exec startx -- -keeptty
+
+        exec sway
     end
 end
