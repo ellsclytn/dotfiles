e693ee13efaf010f6a6acb005cb9087142461b18
Ellis Clayton <ellis@ellis.codes>
2024-11-10 21:07:19 +1100

    wip

--- config/fish/linux.conf.d/firefox.fish
+++ config/fish/linux.conf.d/firefox.fish
@@ -1,2 +1,2 @@
-set -gx MOZ_ENABLE_WAYLAND "0"
-set -gx GDK_BACKEND "x11"
+# set -gx MOZ_ENABLE_WAYLAND "0"
+# set -gx GDK_BACKEND "x11"
--- config/fish/linux.conf.d/startx.fish
+++ config/fish/linux.conf.d/startx.fish
@@ -1,7 +1,10 @@
 if status is-login
-    if test -z "$DISPLAY" -a -z "$INITIAL_SETUP" -a "$XDG_VTNR" = 1
+    if test -z "$WAYLAND_DISPLAY" -a -z "$INITIAL_SETUP" -a "$XDG_VTNR" = 1
         gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
         gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Dark'
         exec startx -- -keeptty
+        # sleep 5
+        # export XDG_CURRENT_DESKTOP=sway
+        # XDG_CURRENT_DESKTOP=sway dbus-run-session sway
     end
 end
