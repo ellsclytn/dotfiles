1cd992bb4b5ad81d8ceb755b581829e3363ef48b
Ellis Clayton <ellis@ellis.codes>
2025-05-14 16:28:27 +1000

    Reconfigure for nvim 0.11/mason 2.0

--- config/nvim/lua/_lsp/init.lua
+++ config/nvim/lua/_lsp/init.lua
@@ -10,33 +10,20 @@ return function()
     local lsp_capabilities = cmp_nvim_lsp.default_capabilities()
 
     vim.lsp.set_log_level('off')
-
     mason.setup()
-    mason_lspconfig.setup({
-        ensure_installed = {
-            'bashls',
-            'gopls',
-            'jsonls',
-            'lua_ls',
-            'terraformls',
-            'tflint',
-            'ts_ls',
-            'yamlls',
-        },
-    })
+    mason_lspconfig.setup()
 
-    mason_lspconfig.setup_handlers({
-        ['rust_analyzer'] = function() end,
-        -- The first entry (without a key) will be the default handler
-        -- and will be called for each installed server that doesn't have
-        -- a dedicated handler.
-        function(server_name)
-            lspconfig[server_name].setup({
+    for _, server in ipairs(mason_lspconfig.get_installed_servers()) do
+        -- Specific rust-analyzer setup.
+        if server == 'rust_analyzer' then
+        -- Other servers.
+        else
+            vim.lsp.config(server, {
                 on_attach = lsp_on_attach,
                 capabilities = lsp_capabilities,
             })
-        end,
-    })
+        end
+    end
 
     require('_lsp.rust')
     require('_lsp.cmp')
