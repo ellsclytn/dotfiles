7b259958776b6e3880180b05e86fe4d549cb44d3
Ellis Clayton <ellis@ellis.codes>
2025-10-05 21:14:20 +1100

    Add xdg-desktop-portal
    It's needed by some applications (e.g. Steam) for opening a file picker.
    Adding xdg-desktop-portal causes some applications (e.g. Firefox) to
    start using the theme and dark mode preference from the portal config,
    which means we need to set those via `gettings`. Only really need to set
    it once, but setting it per-login won't hurt.

--- config/fish/linux.conf.d/startx.fish
+++ config/fish/linux.conf.d/startx.fish
@@ -1,5 +1,7 @@
 if status is-login
     if test -z "$DISPLAY" -a -z "$INITIAL_SETUP" -a "$XDG_VTNR" = 1
+        gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+        gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Dark'
         exec startx -- -keeptty
     end
 end
