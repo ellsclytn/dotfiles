13130470969b91b722fd0493d66fdb7c08d939e5
Ellis Clayton <ellis@ellis.codes>
2026-06-01 14:22:55 +1000

    Switch UI font to Ubuntu

--- config/fish/linux.conf.d/startx.fish
+++ config/fish/linux.conf.d/startx.fish
@@ -1,7 +1,7 @@
 if status is-login
     if test -z "$WAYLAND_DISPLAY" -a -z "$INITIAL_SETUP" -a "$XDG_VTNR" = 1
         gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-        gsettings set org.gnome.desktop.interface font-name 'Source Sans 3 11'
+        gsettings set org.gnome.desktop.interface font-name 'Ubuntu Nerd Font 11'
         gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Dark'
 
         exec sway
